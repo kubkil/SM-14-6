b47070cc130d74b98199a8ecefaa4eb9c2a9d918
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-09 19:23:38 +0200

    add counter component

---
+++ script.js
@@ -0,0 +1,22 @@
+const Counter = React.createClass({
+  getInitialState: function () {
+    return {
+      counter: 0
+    };
+  },
+
+  increment: function () {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+
+  render: function () {
+    return React.createElement('div', { onClick: this.increment },
+      React.createElement('span', {}, 'Licznik ' + this.state.counter)
+    );
+  }
+});
+
+const element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
