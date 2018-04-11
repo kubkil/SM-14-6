20fcabf18dc9843ec5b9b03d7520737178f79c63
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-11 21:31:17 +0200

    create counter

---
+++ counterThree.js
@@ -0,0 +1,31 @@
+const CounterThree = React.createClass({
+  getInitialState: function () {
+    return {
+      counter: 0
+    };
+  },
+
+  increment: function () {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+
+  decrement: function () {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+
+  render: function () {
+    return React.createElement('div', { className: 'container' },
+      React.createElement('button', { onClick: this.increment, className: 'button' }, 'Increment '),
+      React.createElement('button', { onClick: this.decrement, className: 'button' }, 'Decrement '),
+      React.createElement('p', { className: 'number' }, this.state.counter),
+    );
+  }
+
+});
+
+// const elementThree = React.createElement(CounterThree);
+// ReactDOM.render(element, document.getElementById('app-three'));
