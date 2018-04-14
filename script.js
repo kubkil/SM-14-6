b043e9260edc576dc643e1d75384e2183b91d4aa
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-14 18:55:29 +0200

    add create Counter 4 times

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const elementOne = React.createElement(CounterOne);
-ReactDOM.render(elementOne, document.getElementById('app-one'));
-
-const elementTwo = React.createElement(CounterTwo);
-ReactDOM.render(elementTwo, document.getElementById('app-two'));
-
-const elementThree = React.createElement(CounterThree);
-ReactDOM.render(elementThree, document.getElementById('app-three'));
+const counter = React.createElement('div', {},
+  React.createElement(Counter),
+  React.createElement(Counter),
+  React.createElement(Counter),
+  React.createElement(Counter)
+);
+ReactDOM.render(counter, document.getElementById('app'));
